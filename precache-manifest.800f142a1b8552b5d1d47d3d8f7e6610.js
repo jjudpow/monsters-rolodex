256924f506947174737654c2a144887aa9e4e7c7
James Powers <james.j.powers@outlook.com>
2019-10-18 19:37:42 -0400

    Updates

---
+++ precache-manifest.800f142a1b8552b5d1d47d3d8f7e6610.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b289b40f8e0396ea35ff506b7d4ff75",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3c7f675660bf6a722baf",
+    "url": "/monsters-rolodex/static/css/main.735be0ae.chunk.css"
+  },
+  {
+    "revision": "e57ff400467a2f25bbe9",
+    "url": "/monsters-rolodex/static/js/2.1a8f040e.chunk.js"
+  },
+  {
+    "revision": "3c7f675660bf6a722baf",
+    "url": "/monsters-rolodex/static/js/main.4f3c56d4.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
